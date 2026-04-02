1ecd22adcded51f18e299b4da0a4a62a654dfd86
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-04-02 10:53:38 +0200

    Save progress: 2026-04-02

--- Package.swift
+++ Package.swift
@@ -60,9 +60,14 @@ for target in package.targets where ![.system, .binary, .plugin, .macro].contain
         .enableUpcomingFeature("InternalImportsByDefault"),
         .enableUpcomingFeature("MemberImportVisibility"),
         .enableUpcomingFeature("NonisolatedNonsendingByDefault"),
+        .enableExperimentalFeature("LifetimeDependence"),
         .enableExperimentalFeature("Lifetimes"),
         .enableExperimentalFeature("SuppressedAssociatedTypes"),
+        .enableUpcomingFeature("InferIsolatedConformances"),
+        .enableUpcomingFeature("LifetimeDependence"),
     ]
 
-    target.swiftSettings = (target.swiftSettings ?? []) + ecosystem
+    let package: [SwiftSetting] = []
+
+    target.swiftSettings = (target.swiftSettings ?? []) + ecosystem + package
 }
